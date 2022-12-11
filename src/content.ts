3200a975b3d3316f74bada8eaec87d66b8c9f9df
ademidun <tomiademidun@gmail.com>
2022-12-11 08:53:26 -0500

    use a proxy to listen to all function calls to window.ethereum
    Then we can see what functions our browser wallet calls and use that to see which methods we need to implement.

--- src/content.ts
+++ src/content.ts
@@ -1,10 +1,9 @@
-import { Eip1193Bridge } from "@ethersproject/experimental";
 import { ethers } from "ethers";
 import { CHAINS_CONFIG, goerli } from "./models/Chain";
 import { AquaProvider } from "./utils/AquaProvider";
 
-
-
+// use this to keep track of what content script version is being used
+console.log("Content Script version: ", 1);
 
 export function initializeBridge(){
     const chain = CHAINS_CONFIG[goerli.chainId];
@@ -14,9 +13,37 @@ export function initializeBridge(){
     const signer = provider.getSigner();
 
     const bridge = new AquaProvider(signer, provider);
-    
     window.ethereum = bridge;
-    
+
+    window.ethereum = new Proxy(window.ethereum, {
+        // Proxy handler that intercepts function calls and property accesses
+        apply: function(target, thisArg, args: any[]) {
+          // Log the function call
+          console.log(`apply: ${target.name} called with arguments:`, args);
+      
+          // Call the original function
+          return Reflect.apply(target, thisArg, args);
+        },
+      
+        get: function(target, prop) {
+          // Return a function if the property is a function
+          if (typeof target[prop] === "function") {
+            return function(...args: any) {
+              // Log the function call
+              console.log(`get: ${String(prop)} called with arguments:`, args);
+      
+              // Call the original function
+              return target[prop](...args);
+            };
+          }
+      
+          // Return the original property value
+          return target[prop];
+        },
+      });
+
     console.log({provider, signer}, {ethereum: window.ethereum});
 
+
 }
+
--- src/utils/AquaProvider.ts
+++ src/utils/AquaProvider.ts
@@ -9,16 +9,20 @@ export interface RequestArguments {
 export class AquaProvider extends Eip1193Bridge {
 
     request = ({method, params}: RequestArguments): Promise<any> => {
-        if (method === "eth_requestAccounts") {
-            return this.requestAccounts();
-          }
+        try {
+            if (method === "eth_requestAccounts") {
+                return this.requestAccounts();
+              }
+        }
+        catch(error: any) {
+            return Promise.reject(new Error(error));  
+        }
           return Promise.reject(new Error("Invalid method"));
     }
     
     // https://eips.ethereum.org/EIPS/eip-1102
     private requestAccounts = async (): Promise<Array<string>> => {
-
-        const privateKey = ""; 
+        const privateKey = "0x37199c9a3d11079748093d7c64a23293fe2bcef7fcc76b7ed61074354320d8db"; 
         const account = new Wallet(privateKey, this.provider);
         return Promise.resolve([account.address]);
     }

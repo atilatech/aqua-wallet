9eb53abada60abf661d724ed52cc34f0495caf0b
ademidun <tomiademidun@gmail.com>
2022-12-11 09:02:21 -0500

    remove content scripts and craco
    to make version 1 more focused, we should just focus on creating and recovering accounts and sending eth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import { initializeBridge } from './content';
 import Home from './scenes/Home/Home';
 
 function App() {
-
-  useEffect(() => { 
-    initializeBridge();
-  })
   
   return (
     <div className="App">
